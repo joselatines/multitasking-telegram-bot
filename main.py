10d07ad067fd4a0c1c06b6fbe69142638ddd16d6
Jose Latines <joselatines33@gmail.com>
2023-09-19 04:18:54 -0400

    fix: refactor shorten command

--- main.py
+++ main.py
@@ -13,15 +13,20 @@ START_COMMAND = "/start"
 SHORT_COMMAND = "/short"
 ABOUT_COMMAND = "/about"
 
+
 current_cmd = None
 
-@bot.message_handler(commands=[START_COMMAND, 'start',"ayuda", "help"])
+
+@bot.message_handler(commands=[START_COMMAND, "start", "ayuda", "help"])
 def handle_start_command(message):
     """
     Handles the '/start' command by sending a welcome message.
     """
     bot.send_chat_action(message.chat.id, "typing")
-    bot.reply_to(message, f"🤖: Hi I'am a shortener expert, try {SHORT_COMMAND} to see me working!")
+    bot.reply_to(
+        message,
+        f"🤖: Hi I'am a shortener expert, try {SHORT_COMMAND} to see me working!",
+    )
 
 
 @bot.message_handler(commands=[SHORT_COMMAND, "short", "acortar", "shortener"])
@@ -30,12 +35,39 @@ def handle_short_link_command(message):
     Handles the '/short' command by sending a prompt for the link to shorten and setting the current command to 'short'.
     """
     bot.send_chat_action(message.chat.id, "typing")
-    bot.reply_to(
-        message, "🤖: Please insert your link with the short name: www.example.com"
+
+    user_msg = str(message.text)
+
+    full_url = user_msg.replace(SHORT_COMMAND, " ").strip()
+
+    if not full_url.startswith("http://") and not full_url.startswith("https://"):
+        return bot.reply_to(
+            message,
+            "🤖: Please insert a valid URL. Example: https://www.joselatines.com/",
+            disable_web_page_preview=True,
+        )
+
+    bot.reply_to(message, "🤖: Shorting your link...")
+    response_link = shorten_link(full_url)
+
+    error = response_link.get("error")
+    shorted_link = response_link.get("shorted_link")
+
+    if error:
+        return bot.reply_to(
+            message,
+            f"🤖: An error occurred while shortening the link: {error}",
+        )
+
+    bot.send_message(
+        message.chat.id,
+        f"🤖: Here is your shortened link: {shorted_link}",
+        disable_web_page_preview=True,
     )
 
     global current_cmd
     current_cmd = "short"
+
 
 @bot.message_handler(commands=[ABOUT_COMMAND, "about", "acerca", "aboutme"])
 def handle_about_command(message):
@@ -43,9 +75,7 @@ def handle_about_command(message):
     Handles the '/about' command by sending a prompt for the link to shorten and setting the current command to 'short'.
     """
     bot.send_chat_action(message.chat.id, "typing")
-    bot.reply_to(
-        message, "🤖: I am a bot created by the awesome developer José Latines"
-    )
+    bot.reply_to(message, "🤖: I am a bot created by the awesome developer José Latines")
 
 
 @bot.message_handler(content_types=["text"])
@@ -62,8 +92,10 @@ def handle_text_message(message):
         bot.send_message(
             current_chat_id, "🤖: I don't know that command or is not available"
         )
+    else:
+        bot.send_message(current_chat_id, "🤖: I am a bot, use my commands!")
 
-    elif current_cmd == "short":
+    """ elif current_cmd == "short":
         res_shorted_link = shorten_link(message.text)
         error = res_shorted_link["error"]
         if error:
@@ -76,10 +108,8 @@ def handle_text_message(message):
                 f"🤖✅: Here is your link: {res_shorted_link['short_link']}",
             )
 
-            current_cmd = None
-
-    else:
-        bot.send_message(current_chat_id, "🤖: I am a bot, use my commands!")
+            current_cmd = None 
+    """
 
 
 def start_bot():
--- shorter.py
+++ shorter.py
@@ -8,11 +8,14 @@ load_dotenv()
 SHORTER_API_KEY = os.getenv("SHORTER_API_KEY")
 SHORTER_BASE_URL = os.getenv("SHORTER_BASE_URL")
 
-def shorten_link(full_link:str, link_name=""):
+
+def shorten_link(full_link: str, link_name: str = "") -> dict:
+    print(full_link, "shorten links")
     try:
         shorteners = pyshorteners.Shortener(api_key=SHORTER_API_KEY)
-        short_link = shorteners.bitly.short(full_link)
-        return {"short_link": short_link, "error": False}
-   
+        shorted_link = shorteners.bitly.short(full_link)
+        return {"shorted_link": shorted_link, "error": False}
+
     except Exception as e:
-        return {"error": str(e)}
+        print(e)
+        return {"shorted_link": None, "error": str(e)}
